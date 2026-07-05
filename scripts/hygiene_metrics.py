7fb8271440dc0f7d5bbcdfd76c4257942f7ea196
Mahesh1772 <sivagnanam.maheshwaran@gmail.com>
2026-07-05 20:34:14 +0800

    Update hygiene_metrics.py

--- scripts/hygiene_metrics.py
+++ scripts/hygiene_metrics.py
@@ -18,12 +18,13 @@ def setup_environment():
     train_txt = data / Path('corpus/train.txt')
     test_txt = data / Path('corpus/test.txt')
     
-    project_madurai_txt = cleaned_data / Path('project_madurai_extracted.txt')
-    tamil_wiki_txt = cleaned_data / Path('tamil_wiki_extracted.txt')
-    tamil_cc100_txt = cleaned_data / Path('tamil_cc100_extracted.txt')
-    
+    project_madurai_txt = cleaned_data / Path('project_madurai_extracted_above_threshold.txt')
+    tamil_wiki_txt = cleaned_data / Path('tamil_wiki_extracted_above_threshold.txt')
+    tamil_wiki_long_lines_txt = cleaned_data / Path('tamil_wiki_extracted_long_lines_above_threshold.txt')
+    tamil_cc100_txt = cleaned_data / Path('tamil_cc100_extracted_above_threshold.txt')
+
     merged_txt = cleaned_data / Path('merged.txt')
-    file_paths = [project_madurai_txt, tamil_wiki_txt, tamil_cc100_txt, test_txt, train_txt, merged_txt]
+    file_paths = [project_madurai_txt, tamil_wiki_txt, tamil_cc100_txt, test_txt, train_txt, merged_txt, tamil_wiki_long_lines_txt]
 
     hygiene_metrics = [ 'h1_contamination_', 'h2_encoding_anomaly_', 'h3_duplicate_ratio_', 'h5_invalid_lines_' ]
        
